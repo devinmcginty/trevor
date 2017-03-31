aa50d59d389e8be83238501920a0db3b5146bc98
Devin McGinty <dmcginty@civisanalytics.com>
2017-03-31 15:58:03 -0500

    try some shit

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,9 @@
 var TREVOR = 'nope';
 var font='100px Comic Sans MS, cursive, sans-serif';
 
+var shit = $('#shit');
+var showSomeShit = false;
+
 function draw() {
   particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
@@ -21,4 +24,24 @@ function draw() {
                 .text(TREVOR);
     $('#container').append(div)
   }
+
 }
+
+$(document).ready(function(){
+    animateDiv();
+});
+
+function makeNewPosition(){
+    var h = $(window).height() - 100;
+    var w = $(window).width() - 260;
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    return [nh,nw];
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('#shit').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();
+    });
+};
